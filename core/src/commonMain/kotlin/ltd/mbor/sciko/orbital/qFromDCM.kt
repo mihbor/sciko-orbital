5cd45f322f00890ef26edd5648304a7d73392097
Michal Borowiecki <6325352+mihbor@users.noreply.github.com>
2025-11-24 21:23:23 +0000

    simplify qFromDCM.kt

--- core/src/commonMain/kotlin/ltd/mbor/sciko/orbital/qFromDCM.kt
+++ core/src/commonMain/kotlin/ltd/mbor/sciko/orbital/qFromDCM.kt
@@ -1,12 +1,13 @@
 package ltd.mbor.sciko.orbital
 
+import ltd.mbor.sciko.linalg.eig
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.ndarray
 import org.jetbrains.kotlinx.multik.ndarray.data.D1
 import org.jetbrains.kotlinx.multik.ndarray.data.D2
 import org.jetbrains.kotlinx.multik.ndarray.data.MultiArray
 import org.jetbrains.kotlinx.multik.ndarray.data.get
-import kotlin.math.sqrt
+import org.jetbrains.kotlinx.multik.ndarray.operations.div
 
 /**
  * Convert a direction cosine matrix (3x3) to a quaternion (4x1).
@@ -30,59 +31,14 @@ fun qFromDcm(Q: MultiArray<Double, D2>): MultiArray<Double, D1> {
 
     val K3 = mk.ndarray(
         mk[
-            mk[k11 / 3.0, k12 / 3.0, k13 / 3.0, k14 / 3.0],
-            mk[k12 / 3.0, k22 / 3.0, k23 / 3.0, k24 / 3.0],
-            mk[k13 / 3.0, k23 / 3.0, k33 / 3.0, k34 / 3.0],
-            mk[k14 / 3.0, k24 / 3.0, k34 / 3.0, k44 / 3.0]
+            mk[k11, k12, k13, k14],
+            mk[k12, k22, k23, k24],
+            mk[k13, k23, k33, k34],
+            mk[k14, k24, k34, k44]
         ]
-    )
-
-    // Power iteration to find the dominant eigenvector of the symmetric matrix K3
-    val n = 4
-    var v = DoubleArray(n) { 0.0 }
-    v[0] = 1.0 // initial guess (deterministic)
-
-    val maxIter = 1000
-    val tol = 1e-12
-
-    fun matVecMul(mat: MultiArray<Double, D2>, vec: DoubleArray): DoubleArray {
-        val out = DoubleArray(n) { 0.0 }
-        for (i in 0 until n) {
-            var s = 0.0
-            for (j in 0 until n) {
-                s += mat[i, j] * vec[j]
-            }
-            out[i] = s
-        }
-        return out
-    }
-
-    fun norm(vec: DoubleArray): Double {
-        var s = 0.0
-        for (i in vec.indices) s += vec[i] * vec[i]
-        return sqrt(s)
-    }
-
-    var iter = 0
-    while (iter < maxIter) {
-        val w = matVecMul(K3, v)
-        val wnorm = norm(w)
-        if (wnorm == 0.0) break
-        val vNext = DoubleArray(n) { i -> w[i] / wnorm }
-        // compute difference
-        var diff = 0.0
-        for (i in 0 until n) {
-            val d = vNext[i] - v[i]
-            diff += d * d
-        }
-        if (sqrt(diff) < tol) {
-            v = vNext
-            break
-        }
-        v = vNext
-        iter++
-    }
-
-    // Ensure the returned quaternion has the same orientation as MATLAB (4th element is scalar)
-    return mk.ndarray(mk[v[0], v[1], v[2], v[3]])
+    ) / 3.0
+    val (eigval, eigvec) = mk.linalg.eig(K3)
+    val diag = (0..3).map { eigval[it][it] }    // extract diagonal
+    val (i, max) = diag.withIndex().maxBy { it.value } // value and index
+    return eigvec.transpose()[i]
 }
